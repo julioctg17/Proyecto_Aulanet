1d46ccdc82398de75a04dc168da2ab5b967eed9e
Julio César Trinidad García <utp0211635@alumno.utpuebla.edu.mx>
2025-07-12 18:06:24 -0600

    Envio de diseño inicial

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Proyecto_Aulanet;
+
 
 namespace Proyecto_Aulanet
 {
-    public partial class Form1 : Form
+    public partial class frmsesion : Form
     {
-        public Form1()
+        public frmsesion()
         {
             InitializeComponent();
+        }
+
+        private void frmsesion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntcrearcuen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
