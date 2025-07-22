23e47a1e4cfbdf3bcab6e6478dae0e417dfbc096
Julio Trinidad <utp0211635@alumno.utpuebla.edu.mx>
2025-07-21 22:06:39 -0600

    Nueva base de proyecto limpia y funcional

---
+++ frmEscolaridadPre.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Proyecto_Aulanet
+{
+    partial class frmEscolaridadPre
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEscolaridadPre));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.bntsi = new System.Windows.Forms.Button();
+            this.bntno = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.btnregEsco = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(247)))), ((int)(((byte)(245)))));
+            this.label2.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(36)))), ((int)(((byte)(124)))));
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(191, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(667, 40);
+            this.label2.TabIndex = 51;
+            this.label2.Text = "¿Usted tiene alguna escolaridad previa?";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(247)))), ((int)(((byte)(245)))));
+            this.label3.Font = new System.Drawing.Font("Arial", 16F);
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(316, 68);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(445, 32);
+            this.label3.TabIndex = 52;
+            this.label3.Text = "¿Ticchīua in tlatskuiloyan oksepan?";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(16, 273);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(47, 96);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 55;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(1007, 273);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(47, 96);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 56;
+            this.pictureBox8.TabStop = false;
+            // 
+            // bntsi
+            // 
+            this.bntsi.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.bntsi.AutoSize = true;
+            this.bntsi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(36)))), ((int)(((byte)(124)))));
+            this.bntsi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.bntsi.ForeColor = System.Drawing.Color.White;
+            this.bntsi.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.bntsi.Location = new System.Drawing.Point(114, 257);
+            this.bntsi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bntsi.MaximumSize = new System.Drawing.Size(291, 113);
+            this.bntsi.Name = "bntsi";
+            this.bntsi.Size = new System.Drawing.Size(291, 113);
+            this.bntsi.TabIndex = 57;
+            this.bntsi.Text = "         si        \r\n Kemaj";
+            this.bntsi.UseVisualStyleBackColor = false;
+            // 
+            // bntno
+            // 
+            this.bntno.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.bntno.AutoSize = true;
+            this.bntno.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(36)))), ((int)(((byte)(124)))));
+            this.bntno.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.bntno.ForeColor = System.Drawing.Color.White;
+            this.bntno.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.bntno.Location = new System.Drawing.Point(666, 256);
+            this.bntno.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bntno.MaximumSize = new System.Drawing.Size(291, 113);
+            this.bntno.Name = "bntno";
+            this.bntno.Size = new System.Drawing.Size(291, 113);
+            this.bntno.TabIndex = 58;
+            this.bntno.Text = "  No \r\nAmo";
+            this.bntno.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(61, 55);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 59;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(993, 637);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(61, 55);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 60;
+            this.pictureBox6.TabStop = false;
+            // 
+            // btnregEsco
+            // 
+            this.btnregEsco.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnregEsco.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnregEsco.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnregEsco.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(36)))), ((int)(((byte)(124)))));
+            this.btnregEsco.Image = ((System.Drawing.Image)(resources.GetObject("btnregEsco.Image")));
+            this.btnregEsco.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnregEsco.Location = new System.Drawing.Point(33, 634);
+            this.btnregEsco.Margin = new System.Windows.Forms.Padding(4);
+            this.btnregEsco.Name = "btnregEsco";
+            this.btnregEsco.Size = new System.Drawing.Size(163, 57);
+            this.btnregEsco.TabIndex = 61;
+            this.btnregEsco.Text = "  Regresar\r\nTimochīhua";
+            this.btnregEsco.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnregEsco.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnregEsco.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox2.Location = new System.Drawing.Point(377, 377);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(313, 279);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 62;
+            this.pictureBox2.TabStop = false;
+            // 
+            // frmEscolaridadPre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(247)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(1067, 705);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnregEsco);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.bntno);
+            this.Controls.Add(this.bntsi);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1082, 742);
+            this.Name = "frmEscolaridadPre";
+            this.Text = "Aul@Net";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Button bntsi;
+        private System.Windows.Forms.Button bntno;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Button btnregEsco;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
